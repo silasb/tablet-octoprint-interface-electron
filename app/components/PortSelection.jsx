9a0404a1761811f185474243fe84bf098f3e03b2
silasb <silas.baronda@gmail.com>
2015-08-13 20:42:51 -0400

    Adding a quick autoconnect settings boolean

--- app/components/PortSelection.jsx
+++ app/components/PortSelection.jsx
@@ -1,10 +1,34 @@
 var React = require( 'react' );
 
+var remote = require('remote')
+var app = remote.require('app')
+var fs = require( 'fs' );
+
 var PortSelection = React.createClass({
     getInitialState: function() {
-        return {
-            port: null,
-            baud: null
+        var userDataPath = app.getPath('userData');
+
+        var data = JSON.stringify(this.state);
+
+        var data
+        if(fs.existsSync(userDataPath + '/settings.json')) {
+            var json = fs.readFileSync(userDataPath + '/settings.json')
+            data = JSON.parse(json)
+        }
+
+        if (data['on-connect-save-auto-connect']) {
+            this.props.onConnectPrinter(data)
+
+            return {
+                port: null,
+                baud: null,
+            }
+        } else {
+            return {
+                port: null,
+                baud: null,
+                'on-connect-save-auto-connect': false
+            }
         }
     },
 
@@ -24,6 +48,23 @@ var PortSelection = React.createClass({
 
     handleConnect: function() {
         this.props.onConnectPrinter(this.state)
+
+        // need to make sure settings actually worked before we try saving them to the auto connect file.
+        if (this.state['on-connect-save-auto-connect']) {
+            var userDataPath = app.getPath('userData');
+
+            var data = JSON.stringify(this.state);
+
+            fs.writeFile(userDataPath + '/settings.json', data, function(error) {
+                if (error) throw error;
+                console.info('successfully saved settings.json')
+            })
+        }
+    },
+
+    handleAutoConnect: function() {
+        this.setState({'on-connect-save-auto-connect': true})
+
     },
 
     render: function() {
@@ -50,6 +91,10 @@ var PortSelection = React.createClass({
                 </Select>
 
                 <button className="btn btn-primary" onClick={this.handleConnect} disabled={disabled}>Connect</button>
+                <label className="checkbox-inline">
+                    <input type="checkbox" ref="autoconnect" onClick={this.handleAutoConnect} />
+                    Autoconnect
+                </label>
             </div>
         )
     }
